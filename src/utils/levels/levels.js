406cab7d3208f92131ed222f405f32d19d964d2b
Bill Kampouroudis <v.kampouroudis@gmail.com>
2021-01-05 22:49:13 +0200

    Update levels

--- src/utils/levels/levels.js
+++ src/utils/levels/levels.js
@@ -4,14 +4,14 @@ export const levels = (level) => {
   const _levels = [
     { key: 0, minXp: 0 }, // not in use
     { key: 1, minXp: 200, discount: 0.02 },
-    { key: 2, minXp: 400, discount: 0.04 },
-    { key: 3, minXp: 700, discount: 0.05 },
-    { key: 4, minXp: 1000, discount: 0.1 },
-    { key: 5, minXp: 1400, discount: 0.15 },
-    { key: 6, minXp: 2000, discount: 0.2 },
-    { key: 7, minXp: 3000, discount: 0.25 },
-    { key: 8, minXp: 4500, discount: 0.3 },
-    { key: 9, minXp: 6000, discount: 0.5 },
+    { key: 2, minXp: 500, discount: 0.04 },
+    { key: 3, minXp: 1000, discount: 0.05 },
+    { key: 4, minXp: 1500, discount: 0.1 },
+    { key: 5, minXp: 2500, discount: 0.15 },
+    { key: 6, minXp: 3500, discount: 0.2 },
+    { key: 7, minXp: 5000, discount: 0.25 },
+    { key: 8, minXp: 7000, discount: 0.3 },
+    { key: 9, minXp: 9000, discount: 0.5 },
     { key: 10, minXp: 12000, discount: 0.6 }
   ];
 
